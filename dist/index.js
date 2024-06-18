75d09772f011c9bddf90f1fc65da3202917fa264
Nishant-Pall <palln6935@gmail.com>
2024-06-18 15:06:32 +0530

    fix patch, add documentation

--- dist/index.js
+++ dist/index.js
@@ -4,27 +4,6 @@
     if (props === null) props = {};
     return { type, props, children };
   };
-  var render = (node, parent = null) => {
-    if (parent) parent.textContent = "";
-    const mount = parent ? (el) => parent.appendChild(el) : (el) => el;
-    if (typeof node == "string" || typeof node == "number") {
-      return mount(document.createTextNode(node));
-    } else if (typeof node == "boolean" || node === null) {
-      return mount(document.createTextNode(""));
-    } else if (typeof node == "object" && typeof node.type == "function") {
-      return Component.render(node, parent);
-    } else if (typeof node == "object" && typeof node.type == "string") {
-      const dom2 = document.createElement(node.type);
-      for (const child of [
-        /* flatten */
-      ].concat(...node.children))
-        dom2.appendChild(render(child));
-      for (const prop in node.props) setAttribute(dom2, prop, node.props[prop]);
-      return mount(dom2);
-    } else {
-      console.error(`Invalid node: ${node}`);
-    }
-  };
   var setAttribute = (dom2, key, value) => {
     if (typeof value == "function" && key.startsWith("on")) {
       const eventType = key.slice(2).toLowerCase();
@@ -32,11 +11,11 @@
       dom2.removeEventListener(eventType, dom2.__customHandler[eventType]);
       dom2.__customHandler[eventType] = value;
       dom2.addEventListener(eventType, dom2.__customHandler[eventType]);
-    } else if (key === "className" || key === "checked" || key === "value") {
+    } else if (key == "className" || key == "checked" || key == "value") {
       dom2[key] = value;
     } else if (key == "style" && typeof value == "object") {
       Object.assign(dom2.style, value);
-    } else if (key === "ref" && typeof value == "function") {
+    } else if (key == "ref" && typeof value == "function") {
       value(dom2);
     } else if (key == "key") {
       dom2.__customKey = value;
@@ -44,20 +23,37 @@
       dom2.setAttribute(key, value);
     }
   };
+  var render = (vdom, parent = null) => {
+    const mount = parent ? (el) => parent.appendChild(el) : (el) => el;
+    if (typeof vdom == "string" || typeof vdom == "number") {
+      return mount(document.createTextNode(vdom));
+    } else if (typeof vdom == "boolean" || vdom === null) {
+      return mount(document.createTextNode(""));
+    } else if (typeof vdom == "object" && typeof vdom.type == "function") {
+      return Component.render(vdom, parent);
+    } else if (typeof vdom == "object" && typeof vdom.type == "string") {
+      const dom2 = mount(document.createElement(vdom.type));
+      for (const child of [].concat(...vdom.children)) render(child, dom2);
+      for (const prop in vdom.props) setAttribute(dom2, prop, vdom.props[prop]);
+      return mount(dom2);
+    } else {
+      console.error(`Invalid vdom: ${vdom}`);
+    }
+  };
   var patch = (dom2, vdom, parent = dom2.parentNode) => {
-    const replace = parent ? (el) => parent.replaceChild(el, dom2) : (el) => el;
-    if (typeof dom2 == "object" && typeof vdom.type == "function") {
+    const replace = parent ? (el) => parent.replaceChild(el, dom2) && el : (el) => el;
+    if (typeof vdom == "object" && typeof vdom.type == "function") {
       return Component.patch(dom2, vdom, parent);
     } else if (typeof vdom != "object" && dom2 instanceof Text) {
       return dom2.textContent != vdom ? replace(render(vdom, parent)) : dom2;
     } else if (typeof vdom == "object" && dom2 instanceof Text) {
       return replace(render(vdom, parent));
-    } else if (typeof dom2 == "object" && dom2.nodeName != vdom?.type?.toUpperCase()) {
+    } else if (typeof vdom == "object" && dom2.nodeName != vdom?.type?.toUpperCase()) {
       return replace(render(vdom, parent));
     } else if (typeof vdom == "object" && dom2.nodeName == vdom?.type?.toUpperCase()) {
       const pool = {};
       const active = document.activeElement;
-      [].concat(...dom2.children).map((child, index) => {
+      [].concat(...dom2.childNodes).map((child, index) => {
         const key = child.__customKey || `__index__${index}`;
         pool[key] = child;
       });
@@ -68,7 +64,7 @@
       });
       for (const key in pool) {
         const instance = pool[key].__customInstance;
-        if (instance) instance.componentWillUnMount();
+        if (instance) instance.componentWillUnmount();
         pool[key].remove();
       }
       for (const attr of dom2.attributes) dom2.removeAttribute(attr.name);
@@ -80,7 +76,7 @@
 
   // src/index.jsx
   var oldList = /* @__PURE__ */ createElement("ul", { className: "some-list" }, /* @__PURE__ */ createElement("li", { className: "some-list__item", key: "one" }, "One"), /* @__PURE__ */ createElement("li", { className: "some-list__item", key: "two" }, "Two"));
-  var newList = /* @__PURE__ */ createElement("ul", { className: "some-list" }, /* @__PURE__ */ createElement("li", { className: "some-list__item", key: "three" }, "Three"), /* @__PURE__ */ createElement("li", { className: "some-list__item", key: "two" }, "Two"));
+  var newList = /* @__PURE__ */ createElement("ul", { className: "some-list" }, /* @__PURE__ */ createElement("li", { className: "some-list__item", key: "one" }, "Four"), /* @__PURE__ */ createElement("li", { className: "some-list__item", key: "three" }, "Three"), /* @__PURE__ */ createElement("li", { className: "some-list__item", key: "two" }, "Two"));
   var dom = render(oldList, document.getElementById("root"));
   console.log("dom :>> ", dom);
   patch(dom, newList);
--- src/renderer.js
+++ src/renderer.js
@@ -1,5 +1,6 @@
 /** @jsx createElement */
 export const createElement = (type, props, ...children) => {
+  // for safety
   if (props === null) props = {};
   return { type, props, children };
 };
@@ -17,161 +18,117 @@ function legacyRender(vnode) {
   return node;
 }
 
-export const render = (node, parent = null) => {
-  if (parent) parent.textContent = "";
-  const mount = parent ? (el) => parent.appendChild(el) : (el) => el;
-  if (typeof node == "string" || typeof node == "number") {
-    return mount(document.createTextNode(node));
-  } else if (typeof node == "boolean" || node === null) {
-    return mount(document.createTextNode(""));
-  } else if (typeof node == "object" && typeof node.type == "function") {
-    return Component.render(node, parent);
-  } else if (typeof node == "object" && typeof node.type == "string") {
-    const dom = document.createElement(node.type);
-    for (const child of [
-      /* flatten */
-    ].concat(...node.children))
-      dom.appendChild(render(child));
-    for (const prop in node.props) setAttribute(dom, prop, node.props[prop]);
-    return mount(dom);
-  } else {
-    console.error(`Invalid node: ${node}`);
-  }
-};
-
-const setAttribute = (dom, key, value) => {
+export const setAttribute = (dom, key, value) => {
+  // handling event listeners, onClick will be click etc
   if (typeof value == "function" && key.startsWith("on")) {
     const eventType = key.slice(2).toLowerCase();
     dom.__customHandler = dom.__customHandler || {};
     dom.removeEventListener(eventType, dom.__customHandler[eventType]);
     dom.__customHandler[eventType] = value;
     dom.addEventListener(eventType, dom.__customHandler[eventType]);
-  } else if (key === "className" || key === "checked" || key === "value") {
+  }
+  //  if generic attribute
+  else if (key == "className" || key == "checked" || key == "value") {
     dom[key] = value;
-  } else if (key == "style" && typeof value == "object") {
+  }
+  //  set style of dom node
+  else if (key == "style" && typeof value == "object") {
     Object.assign(dom.style, value);
-  } else if (key === "ref" && typeof value == "function") {
+  }
+  // handling ref
+  else if (key == "ref" && typeof value == "function") {
     value(dom);
-  } else if (key == "key") {
+  }
+  // setting key
+  else if (key == "key") {
     dom.__customKey = value;
-  } else if (typeof value != "object" && typeof value != "function") {
+  }
+  //  miscellaneous attributes
+  else if (typeof value != "object" && typeof value != "function") {
     dom.setAttribute(key, value);
   }
 };
 
+export const render = (vdom, parent = null) => {
+  // if parent exist for the node, then append the node as its child else return the node itself
+  const mount = parent ? (el) => parent.appendChild(el) : (el) => el;
+
+  // if node is normal text, mount a text node
+  if (typeof vdom == "string" || typeof vdom == "number") {
+    return mount(document.createTextNode(vdom));
+  }
+  // if node is null or boolean, mount an empty text node
+  else if (typeof vdom == "boolean" || vdom === null) {
+    return mount(document.createTextNode(""));
+  }
+  // if node is a function (imported component)
+  else if (typeof vdom == "object" && typeof vdom.type == "function") {
+    return Component.render(vdom, parent);
+  }
+  // if node is of type {type, props, children}, i.e. it has children of its own
+  else if (typeof vdom == "object" && typeof vdom.type == "string") {
+    // create a dom object of the node
+    const dom = mount(document.createElement(vdom.type));
+
+    // flatten array of children, this is to handle a case of an array passed as a child
+    // if jsx has {...nodeChildren, [1,2,3,4,5]}, it will create something like {...nodeChildre, 1, 2, 3, 4, 5}
+    // render child and append it to the dom
+    for (const child of [].concat(...vdom.children)) render(child, dom);
+
+    // after rendering, set attributes for the dom object
+    for (const prop in vdom.props) setAttribute(dom, prop, vdom.props[prop]);
+
+    return mount(dom);
+  } else {
+    console.error(`Invalid vdom: ${vdom}`);
+  }
+};
+
 export const patch = (dom, vdom, parent = dom.parentNode) => {
-  const replace = parent ? (el) => parent.replaceChild(el, dom) : (el) => el;
-  if (typeof dom == "object" && typeof vdom.type == "function") {
+  // if parent exists, replace the child, or just return the node itself
+  const replace = parent ? (el) => parent.replaceChild(el, dom) && el : (el) => el;
+  // if node is of type function(imported component)
+  if (typeof vdom == "object" && typeof vdom.type == "function") {
     return Component.patch(dom, vdom, parent);
-  } else if (typeof vdom != "object" && dom instanceof Text) {
+  }
+  //  if both are text content, just replace if they differ else return the node itself
+  else if (typeof vdom != "object" && dom instanceof Text) {
     return dom.textContent != vdom ? replace(render(vdom, parent)) : dom;
-  } else if (typeof vdom == "object" && dom instanceof Text) {
+  }
+  //  if vdom is an object, but dom is text, then replace
+  else if (typeof vdom == "object" && dom instanceof Text) {
     return replace(render(vdom, parent));
-  } else if (typeof dom == "object" && dom.nodeName != vdom?.type?.toUpperCase()) {
+  }
+  //  if both are object but nodes differ
+  else if (typeof vdom == "object" && dom.nodeName != vdom?.type?.toUpperCase()) {
     return replace(render(vdom, parent));
-  } else if (typeof vdom == "object" && dom.nodeName == vdom?.type?.toUpperCase()) {
+  }
+  //  if same type of node
+  else if (typeof vdom == "object" && dom.nodeName == vdom?.type?.toUpperCase()) {
     const pool = {};
+    // maintain active element
     const active = document.activeElement;
-    [].concat(...dom.children).map((child, index) => {
+    // create a pool of children of the node using key if it exists, else use index
+    [].concat(...dom.childNodes).map((child, index) => {
       const key = child.__customKey || `__index__${index}`;
       pool[key] = child;
     });
     [].concat(...vdom.children).map((child, index) => {
       const key = (child.props && child.props.key) || `__index__${index}`;
+      // if pool already has key, just patch again else render because key doesnt exist
       dom.appendChild(pool[key] ? patch(pool[key], child) : render(child, dom));
       delete pool[key];
     });
+    // remove unused nodes
     for (const key in pool) {
       const instance = pool[key].__customInstance;
-      if (instance) instance.componentWillUnMount();
+      if (instance) instance.componentWillUnmount();
       pool[key].remove();
     }
+    // apply new attributes
     for (const attr of dom.attributes) dom.removeAttribute(attr.name);
     for (const prop in vdom.props) setAttribute(dom, prop, vdom.props[prop]);
     active.focus();
     return dom;
   }
 };
-
-// /* Scroll down to reach playground: */
-// /** @jsx createElement */
-// const createElement = (type, props, ...children) => {
-//   if (props === null) props = {};
-//   return { type, props, children };
-// };
-
-// const setAttribute = (dom, key, value) => {
-//   if (typeof value == "function" && key.startsWith("on")) {
-//     const eventType = key.slice(2).toLowerCase();
-//     dom.__gooactHandlers = dom.__gooactHandlers || {};
-//     dom.removeEventListener(eventType, dom.__gooactHandlers[eventType]);
-//     dom.__gooactHandlers[eventType] = value;
-//     dom.addEventListener(eventType, dom.__gooactHandlers[eventType]);
-//   } else if (key == "checked" || key == "value" || key == "className") {
-//     dom[key] = value;
-//   } else if (key == "style" && typeof value == "object") {
-//     Object.assign(dom.style, value);
-//   } else if (key == "ref" && typeof value == "function") {
-//     value(dom);
-//   } else if (key == "key") {
-//     dom.__gooactKey = value;
-//   } else if (typeof value != "object" && typeof value != "function") {
-//     dom.setAttribute(key, value);
-//   }
-// };
-
-// const render = (vdom, parent = null) => {
-//   console.log("render", vdom);
-//   const mount = parent ? (el) => parent.appendChild(el) : (el) => el;
-//   if (typeof vdom == "string" || typeof vdom == "number") {
-//     return mount(document.createTextNode(vdom));
-//   } else if (typeof vdom == "boolean" || vdom === null) {
-//     return mount(document.createTextNode(""));
-//   } else if (typeof vdom == "object" && typeof vdom.type == "function") {
-//     return Component.render(vdom, parent);
-//   } else if (typeof vdom == "object" && typeof vdom.type == "string") {
-//     const dom = mount(document.createElement(vdom.type));
-//     for (const child of [
-//       /* flatten */
-//     ].concat(...vdom.children))
-//       render(child, dom);
-//     for (const prop in vdom.props) setAttribute(dom, prop, vdom.props[prop]);
-//     return dom;
-//   } else {
-//     throw new Error(`Invalid VDOM: ${vdom}.`);
-//   }
-// };
-
-// const patch = (dom, vdom, parent = dom.parentNode) => {
-//   const replace = parent ? (el) => parent.replaceChild(el, dom) && el : (el) => el;
-//   if (typeof vdom == "object" && typeof vdom.type == "function") {
-//     return Component.patch(dom, vdom, parent);
-//   } else if (typeof vdom != "object" && dom instanceof Text) {
-//     return dom.textContent != vdom ? replace(render(vdom, parent)) : dom;
-//   } else if (typeof vdom == "object" && dom instanceof Text) {
-//     return replace(render(vdom, parent));
-//   } else if (typeof vdom == "object" && dom.nodeName != vdom.type.toUpperCase()) {
-//     return replace(render(vdom, parent));
-//   } else if (typeof vdom == "object" && dom.nodeName == vdom.type.toUpperCase()) {
-//     const pool = {};
-//     const active = document.activeElement;
-//     [].concat(...dom.childNodes).map((child, index) => {
-//       const key = child.__gooactKey || `__index_${index}`;
-//       pool[key] = child;
-//     });
-//     [].concat(...vdom.children).map((child, index) => {
-//       const key = (child.props && child.props.key) || `__index_${index}`;
-//       dom.appendChild(pool[key] ? patch(pool[key], child) : render(child, dom));
-//       delete pool[key];
-//     });
-//     for (const key in pool) {
-//       const instance = pool[key].__gooactInstance;
-//       if (instance) instance.componentWillUnmount();
-//       pool[key].remove();
-//     }
-//     for (const attr of dom.attributes) dom.removeAttribute(attr.name);
-//     for (const prop in vdom.props) setAttribute(dom, prop, vdom.props[prop]);
-//     active.focus();
-//     return dom;
-//   }
-// };
